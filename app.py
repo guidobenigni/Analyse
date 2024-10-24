029a0d13516750286420c92b9773c3268e9379e7
guidobenigni <guidogeza@gmail.com>
2024-10-24 12:38:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import pdfplumber
 import re
+import os
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 def extract_swimmers(file):
     with pdfplumber.open(file) as pdf:
@@ -19,15 +24,21 @@ def extract_swimmers(file):
     return swimmer_details
 
 @app.route('/upload', methods=['POST'])
-def upload_pdf():
+def upload():
     if 'pdf' not in request.files:
         return jsonify({'error': 'Kein PDF gefunden'}), 400
 
     file = request.files['pdf']
-    swimmer_details = extract_swimmers(file)
-    result = f'Gefundene Schwimmer: {len(swimmer_details)}.\nDetails:\n' + '\n'.join(swimmer_details)
     
-    return jsonify({'result': result})
+    if file.filename == '':
+        return jsonify({'error': 'Keine Datei ausgewählt'}), 400
+    
+    if file and file.filename.lower().endswith('.pdf'):
+        swimmer_details = extract_swimmers(file)
+        result = f'Gefundene Schwimmer: {len(swimmer_details)}.\nDetails:\n' + '\n'.join(swimmer_details)
+        return jsonify({'result': result})
+    else:
+        return jsonify({'error': 'Bitte eine gültige PDF-Datei hochladen.'}), 400
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0', port=os.getenv('PORT', 5000))
